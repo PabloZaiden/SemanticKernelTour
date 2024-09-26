1809650e30c4024a5fc0c08ce7988494ef1a06cf
Pablo Zaidenvoren <pablozaiden@users.noreply.github.com>
2024-09-26 13:50:17 -0400

    1. create kernel

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using SemanticKernelTour;
+
+var model = Config.Get("Model");
+var openAIAPIKey = Config.Get("OpenAIAPIKey");
+
+var builder = Kernel.CreateBuilder();
+
+builder.AddOpenAIChatCompletion(model, openAIAPIKey);
+builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+var kernel = builder.Build();
